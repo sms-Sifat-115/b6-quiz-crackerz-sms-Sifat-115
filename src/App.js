5fb54b6f56f537439495048c348163433c7c4dee
S M Sanbim Sifat <79338515+sms-Sifat-115@users.noreply.github.com>
2022-10-11 10:17:43 +0600

    Second Draft: Routes Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './Components/Main/Main'
 
 function App() {
+  const router = createBrowserRouter ([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path:'/',
+          element: <Home></Home>
+        },
+        {
+         path: '/topics',
+         element: <Topics></Topics> 
+        },
+        {
+         path: '/stats',
+         element: <Stats></Stats>   
+        },
+        {
+         path: '/blogs',
+         element: <Blogs></Blogs>   
+        },
+
+      ]
+    }
+])
   return (
-    <div className="App">
-      
+    <div className="App dark:bg-slate-900 bg-white">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
